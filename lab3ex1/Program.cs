81f8d10d43b8fa541e6a012109de5c4fd6a41ed9
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-09 18:54:07 +0300

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Scrieti o functie care va calcula suma cifrelor unui numar.*/
+
+            Console.WriteLine("Introduceti numarul:");
+
+            int numarIntrodus = int.Parse(Console.ReadLine());
+
+            int sumaNumComp = SumaCifreNumar(numarIntrodus);
+
+            Console.WriteLine($"Suma cifrelor numarului introdus este: {sumaNumComp}.");
+
+        }
+
+        static int SumaCifreNumar(int numarIntrodus)
+        {
+            int sumNum = 0;
+
+            while (numarIntrodus > 0)
+
+            {
+                int extrUltimaCifra = numarIntrodus % 10;
+                sumNum = sumNum + extrUltimaCifra;
+                numarIntrodus = numarIntrodus / 10;
+
+            }
+            return sumNum;
+        }
+    }
+}  
+
